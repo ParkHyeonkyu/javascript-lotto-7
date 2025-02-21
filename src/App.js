8876f408e210fc153edaaa15861a6d0f38298d48
ParkHyeonkyu <162525426+ParkHyeonkyu@users.noreply.github.com>
2025-02-21 12:38:28 +0900

    feat: 로또 구매 구현
    입출력 구현으로 가격 구매 및 예외 처리 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { Console } from "@woowacourse/mission-utils"
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const purchaseAmount = await this.getPurchaseAmount();
+      const lottoCount = purchaseAmount / 1000;
+
+      Console.print(`${lottoCount}개를 구매했습니다.`);
+
+    } catch (error) {
+      Console.print(error.message);
+    }
+  }
+  async getPurchaseAmount() {
+    const input = await Console.readLineAsync(`구입 금액을 입력해주세요.\n`);
+    const amount = parseInt(input);
+
+    if (isNaN(amount) || amount % 1000 !== 0) {
+      throw new Error("[ERROR] 천 원 단위로 입력해야 합니다.");
+    }
+    return amount;
+  }
 }
 
 export default App;
