c9a9008f2fa3a6dbba64bc6669ebbb5c346a6a86
ParkHyeonkyu <162525426+ParkHyeonkyu@users.noreply.github.com>
2025-02-21 12:05:01 +0900

    feat:Lotto 클래스 구현
    예외를 잡아 줄 Lotto.js와 LottoTest.js 코드 임시 작성

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+import { Random } from "@woowacourse/mission-utils";
+
 class Lotto {
   #numbers;
 
@@ -10,9 +12,30 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    this.#checkNumberRange(numbers);
+    this.#checkDuplicates(numbers);
   }
 
-  // TODO: 추가 기능 구현
+  #checkNumberRange(numbers) {
+    if (!numbers.every(num => num >= 1 && num <= 45)) {
+      throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    }
+  }
+
+  #checkDuplicates(numbers) {
+    const uniqueNumbers = new Set(numbers);
+    if (uniqueNumbers.size !== 6) {
+      throw new Error("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
+    }
+  }
+
+  static generateRandomLotto() {
+    return new Lotto(Random.pickUniqueNumbersInRange(1, 45, 6));
+  }
+
+  getNumbers() {
+    return [...this.#numbers].sort((a, b) => a - b);
+  }
 }
 
 export default Lotto;
